e74086982a9bc26eae9c66884af59b7ac43ec626
VersettiDev <brunomuryllo.nt@icloud.com>
2025-05-24 03:07:33 -0300

    Add LoanType enum and basic Loan class structure
    Introduce the LoanType enum to represent loan categories and a Loan class with a reference to a Customer. These additions lay the foundation for the loan domain model.

---
+++ src/main/java/com/versetti/Loan/Challenge/domain/Loan/Loan.java
@@ -0,0 +1,7 @@
+package com.versetti.Loan.Challenge.domain.Loan;
+
+import com.versetti.Loan.Challenge.domain.Customer.Customer;
+
+public class Loan {
+    private Customer customer;
+}
---
+++ src/main/java/com/versetti/Loan/Challenge/domain/Loan/LoanType.java
@@ -0,0 +1,7 @@
+package com.versetti.Loan.Challenge.domain.Loan;
+
+public enum LoanType {
+    PERSONAL,
+    GUARANTED,
+    CONSIGNMENT
+}

128870fd370e1039e7acae65ea5e526a2342c6f0
VersettiDev <brunomuryllo.nt@icloud.com>
2025-05-24 03:13:05 -0300

    Add LoanService, LoanController, and Lombok to Customer
    Introduced LoanService and LoanController classes as part of the project structure. Added Lombok annotations to the Customer class for improved code simplicity and reduced boilerplate. These changes set up the foundation for loan-related functionality.

---
+++ src/main/java/com/versetti/Loan/Challenge/controller/LoanController.java
@@ -0,0 +1,4 @@
+package com.versetti.Loan.Challenge.controller;
+
+public class LoanController {
+}
---
+++ src/main/java/com/versetti/Loan/Challenge/service/LoanService.java
@@ -0,0 +1,7 @@
+package com.versetti.Loan.Challenge.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class LoanService {
+}

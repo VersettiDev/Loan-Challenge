043cd71a8105b5506214b2d8516c17ce56e19e08
VersettiDev <brunomuryllo.nt@icloud.com>
2025-05-24 03:24:24 -0300

    Add loan type and interest calculation to LoanService
    Introduced LoanType and LoanResponse handling to the LoanService. Updated the `getLoansAvaibleToCustomer` method to calculate and return available loans with their corresponding interest rates. This sets up a foundation for more detailed loan offerings.

--- src/main/java/com/versetti/Loan/Challenge/service/LoanService.java
+++ src/main/java/com/versetti/Loan/Challenge/service/LoanService.java
@@ -1,15 +1,25 @@
 package com.versetti.Loan.Challenge.service;
 
 import com.versetti.Loan.Challenge.domain.Loan.Loan;
+import com.versetti.Loan.Challenge.domain.Loan.LoanType;
 import com.versetti.Loan.Challenge.dto.CLoanRequest;
 import com.versetti.Loan.Challenge.dto.CLoanResponse;
+import com.versetti.Loan.Challenge.dto.LoanResponse;
 import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Service
 public class LoanService {
     public CLoanResponse getLoansAvaibleToCustomer (CLoanRequest loanRequest) {
         var Customer = loanRequest.getCustomer();
         var loan = new Loan(Customer);
+
+        List<LoanResponse> loans = new ArrayList<>();
+        if (loan.isPersonalLoanAllowed()) {
+            loans.add(new LoanResponse(LoanType.PERSONAL, loan.getPersonalInterest()));
+        }
         return null;
     }
 }

05c769c77bdf47f565eaa2d35406440c9c0c4c9f
VersettiDev <brunomuryllo.nt@icloud.com>
2025-05-24 03:06:40 -0300

    Add Customer domain class to represent customer data
    This class introduces key attributes like name, document, age, income, and location for managing customer information. It provides a foundation for future customer-related functionality in the application.

---
+++ src/main/java/com/versetti/Loan/Challenge/domain/Customer/Customer.java
@@ -0,0 +1,11 @@
+package com.versetti.Loan.Challenge.domain.Customer;
+
+public class Customer {
+
+    private String name;
+    private String document;
+    private Integer age;
+
+    private Double income;
+    private String location;
+}

24919c25416cc291aecf13744ec52f31f69a4bf7
VersettiDev <brunomuryllo.nt@icloud.com>
2025-05-24 03:21:09 -0300

    Add validation for personal loan eligibility
    Introduced a method to check if a personal loan is allowed based on customer income, age, and location. Used Lombok annotations for cleaner and more maintainable code.

--- src/main/java/com/versetti/Loan/Challenge/domain/Loan/Loan.java
+++ src/main/java/com/versetti/Loan/Challenge/domain/Loan/Loan.java
@@ -1,7 +1,28 @@
 package com.versetti.Loan.Challenge.domain.Loan;
 
 import com.versetti.Loan.Challenge.domain.Customer.Customer;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
 
 public class Loan {
     private Customer customer;
+
+    public boolean isPersonalLoanAllowed () {
+        if (customer.isIncomeEqualOrLower(3000.0)) {
+            return true;
+        }
+
+        return (
+                customer.isIncomeBetween(3000.0, 5000.0)
+                        && customer.isAgeLowerThan(30)
+                        && customer.isLocationAllowed("SP")
+        );
+    }
 }
